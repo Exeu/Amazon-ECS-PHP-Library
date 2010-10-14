b2094baf721add1dfb3c84549b387c05cee5fc3c
Jan Eichhorn <jan@pixel-web.org>
2010-10-14 23:12:28 +0200

    - Codes

--- test.php
+++ test.php
@@ -8,7 +8,7 @@ require 'lib/AmazonECS.class.php';
 
 try
 {
-    $amazonEcs = new AmazonECS("AKIAJE5FDEZCRP3YZZDQ", "U2O/i63B/PnuKDQiD8p3oWsOZpxQI6Fqp+kMus9R");
+    $amazonEcs = new AmazonECS("", "");
     $response = $amazonEcs->category('DVD')->responseGroup('Images')->search("Matrix Revolutions");
 
     // vardumping the response
