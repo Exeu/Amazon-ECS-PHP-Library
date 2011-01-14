213555859017859a4658f5ed98b493ee256b59d7
Jan Eichhorn <jan@pixel-web.org>
2011-01-14 19:23:52 +0100

    New Samplefile for Node Browsing

---
+++ samples/sampleBrowseNode.php
@@ -0,0 +1,31 @@
+<?php
+if ("cli" !== PHP_SAPI)
+{
+    echo "<pre>";
+}
+
+
+if (is_file('sampleSettings.php'))
+{
+  include 'sampleSettings.php';
+}
+
+defined('AWS_API_KEY') or define('AWS_API_KEY', 'API KEY');
+defined('AWS_API_SECRET_KEY') or define('AWS_API_SECRET_KEY', 'SECRET KEY');
+
+require '../lib/AmazonECS.class.php';
+
+try
+{
+    $amazonEcs = new AmazonECS(AWS_API_KEY, AWS_API_SECRET_KEY, 'DE');
+
+}
+catch(Exception $e)
+{
+  echo $e->getMessage();
+}
+
+if ("cli" !== PHP_SAPI)
+{
+    echo "</pre>";
+}
