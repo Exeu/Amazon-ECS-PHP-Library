9030053176aec97a624f8a86b84e5973593a9e57
Exeu <jan@pixel-web.org>
2012-09-16 10:50:47 +0200

    Bumped Version number

--- lib/AmazonECS.class.php
+++ lib/AmazonECS.class.php
@@ -12,7 +12,7 @@
  *
  * @package      AmazonECS
  * @license      http://www.gnu.org/licenses/gpl.txt GPL
- * @version      1.3.3
+ * @version      1.3.4-DEV
  * @author       Exeu <exeu65@googlemail.com>
  * @contributor  Julien Chaumond <chaumond@gmail.com>
  * @link         http://github.com/Exeu/Amazon-ECS-PHP-Library/wiki Wiki
